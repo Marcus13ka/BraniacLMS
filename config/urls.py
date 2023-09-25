5b8a36c5d0c3191f4b9fc61e18ce6875f4064344
Marcus <Marcus.8sk@gmail.com>
2023-09-25 15:10:12 +0300

    lesson_2 Static site with Django

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='mainapp/')),
